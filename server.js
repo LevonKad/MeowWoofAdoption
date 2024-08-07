7781b20f3a0b43f46fe13b4c893b9bd2fa0bd543
LevonKad <levonkadehjian20@gmail.com>
2024-08-07 13:15:15 -0400

    Update server.js

--- server.js
+++ server.js
@@ -191,7 +191,7 @@ const server = http.createServer((req, res) => {
 });
 
 // Start server
-const port = 3027;
+const port = process.env.PORT || 3000;
 server.listen(port, () => {
-    console.log(`Server is running on port http://localhost:${port}`);
+  console.log('Server is running on http://localhost:' + port);
 });
